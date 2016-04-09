1ae053bab611fe7ad80dceb4308e393f8220426f
JosephineChow <JosephineChow>
2016-04-09 17:19:44 -0400

    modified inputsData.m

--- inputData.m
+++ inputData.m
@@ -4,38 +4,34 @@ function [name, price, purchase, date, endDate] = inputData()
 num = input('Enter the number of stocks you would like to see: ');
 
 name = zeros(num,4);
-price = zeros(num,1);
-purchase = zeros(num,3);
-date = zeros(num,3);
-endDate = zeros(num,3);
+purchase = zeros(num,1);
+date = zeros(num,11);
+endDate = zeros(num,11);
 
 for n = 1:num
     str = input('Enter Stock name: ', 's');
-    name(n, :) = str(:);
+    if (length(str) == 3)
+        name(n, 1:3) = str(:);
+    else
+       name(n,:) = str(:); 
+    end
     
-    p = input('Enter your purchase price: $');
-    price(n,1) = p;
     
-    y = input('Enter your purchased year: ');
-    m = input('Enter your purchased month: ');
-    d = input('Enter your purchased day: ');
-    purchase(n,1) = y;
-    purchase(n,2) = m;
-    purchase(n,3) = d;
+    y = input('Enter your purchased year (ex. 97): ');
+    m = input('Enter your purchased month (ex. 01): ');
+    d = input('Enter your purchased day (ex. 01): ');
+    datetime(y,m,d)
+    purchase(n,1) = dateime(y,m,d);
     
-    y = input('Enter your start year: ');
-    m = input('Enter your start month: ');
-    d = input('Enter your start day: ');
-    date(n,1) = y;
-    date(n,2) = m;
-    date(n,3) = d;
+    y = input('Enter your start year (ex. 97): ');
+    m = input('Enter your start month (ex. 01): ');
+    d = input('Enter your start day (ex. 01): ');
+    date(n,:) = datetime(y,m,d);
     
-    y = input('Enter your end year: ');
-    m = input('Enter your end month: ');
-    d = input('Enter your end day: ');
-    endDate(n,1) = y;
-    endDate(n,2) = m;
-    endDate(n,3) = d;
+    y = input('Enter your end year (ex. 97): ');
+    m = input('Enter your end month (ex. 01): ');
+    d = input('Enter your end day (ex. 01): ');
+    endDate(n,1) = datetime(y,m,d);
 end 
 
 end
