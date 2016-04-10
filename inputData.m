90a6d5956b32a3d1d6915dc764de7a34509b26e7
JosephineChow <JosephineChow>
2016-04-09 20:35:23 -0400

    input data updated

--- inputData.m
+++ inputData.m
@@ -12,6 +12,7 @@ for n = 1:num
     str = input('Enter Stock name: ', 's');
     if (length(str) == 3)
         name(n, 1:3) = str(:);
+        name(n,4) = ' ';
     else
        name(n,:) = str(:); 
     end
@@ -22,18 +23,23 @@ for n = 1:num
     y = input('Enter your purchased year (ex. 97): ');
     m = input('Enter your purchased month (ex. 01): ');
     d = input('Enter your purchased day (ex. 01): ');
-    datetime(y,m,d)
-    purchase(n,1) = dateime(y,m,d);
+    purchase(n,1) = y;
+    purchase(n,2) = m;
+    purchase(n,3) = d;
     
     y = input('Enter your start year (ex. 97): ');
     m = input('Enter your start month (ex. 01): ');
     d = input('Enter your start day (ex. 01): ');
-    date(n,:) = datetime(y,m,d);
-    
+    date(n,1) = y;
+    date(n,2) = m;
+    date(n,3) = d;
+
     y = input('Enter your end year (ex. 97): ');
     m = input('Enter your end month (ex. 01): ');
     d = input('Enter your end day (ex. 01): ');
-    endDate(n,1) = datetime(y,m,d);
+    endDate(n,1) = y;
+    endDate(n,2) = m;
+    endDate(n,3) = d;
 end 
 
 end
