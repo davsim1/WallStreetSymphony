82136de17af27c2d0603cb5b90c67ee3ef8e353d
JosephineChow <JosephineChow>
2016-04-09 20:51:46 -0400

    updated main function

--- main.m
+++ main.m
@@ -3,9 +3,14 @@ import plot.*
 import retStocks.*
 %input musicMaker.*
 
-[names, prices, purchasedPrices, startDates, endDates] = input()
-[stockNames, data] = retStocks(stockNames, startDates, endDates);
-plotStocks(stockNames, data, purchasedPrices)
+[names, prices, purchasedDate, start, endD] = input();
+[m,n] = size(prices); %only m is useful here
+for n = 1:m
+    [stockNames, data] = retStocks(names(n,:), datetime(start(n,1), start(n,2), start(n,3)), datetime(endD(n,1), endD(n,2), endD(n,3)));
+    plotStocks(stockNames, data, prices(n,:));
+
+end
+%[stockNames, data] = retStocks(stockNames, startDates, endDates);
 %makeMusic(data,purchasedPrices)
 
 
