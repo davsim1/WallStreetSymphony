57ac9c000930d70c52960c6ff038f625728ef8f8
JosephineChow <JosephineChow>
2016-04-09 20:24:27 -0400

    david edits mina

--- main.m
+++ main.m
@@ -3,7 +3,11 @@ import plot.*
 import retStocks.*
 %input musicMaker.*
 
-[names, prices, purchasedPrices, startDates, endDates]
+[names, prices, purchasedPrices, startDates, endDates] = input()
+[stockNames, data] = retStocks(stockNames, startDates, endDates);
+plotStocks(stockNames, data, purchasedPrices)
+%makeMusic(data,purchasedPrices)
+
 
 
 
